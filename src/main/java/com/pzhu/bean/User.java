8b9a61c94fa9c8e1777b05ff6e8b99ef8f5b94d7
lyh <3305728541@qq.com>
2025-06-17 20:01:07 +0800

    将telephone属性改为string

--- src/main/java/com/pzhu/bean/User.java
+++ src/main/java/com/pzhu/bean/User.java
@@ -1,28 +1,100 @@
- package com.pzhu.bean;
+// package com.pzhu.bean;
+//
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class User {
+//	private	int uid;
+//	private	int utelephone;
+//	private	String uname;
+//	private	String upartment;
+//	private	String upassword;
+//	private	String type;
+//	private	String sex;
+//	private	int ugrade;
+//
+//	public int getUid() {
+//		return uid;
+//	}
+//	public void setUid(int uid) {
+//		this.uid = uid;
+//	}
+//	public int getUtelephone() {
+//		return utelephone;
+//	}
+//	public void setUtelephone(int utelephone) {
+//		this.utelephone = utelephone;
+//	}
+//	public String getUname() {
+//		return uname;
+//	}
+//	public void setUname(String uname) {
+//		this.uname = uname;
+//	}
+//	public String getUpartment() {
+//		return upartment;
+//	}
+//	public void setUpartment(String upartment) {
+//		this.upartment = upartment;
+//	}
+//	public String getUpassword() {
+//		return upassword;
+//	}
+//	public void setUpassword(String upassword) {
+//		this.upassword = upassword;
+//	}
+//	public String getType() {
+//		return type;
+//	}
+//	public void setType(String type) {
+//		this.type = type;
+//	}
+//
+//	public String getSex() {
+//		return sex;
+//	}
+//	public void setSex(String sex) {
+//		this.sex = sex;
+//	}
+//
+//	public int getUgrade() {
+//		return ugrade;
+//	}
+//	public void setUgrade(int ugrade) {
+//		this.ugrade = ugrade;
+//	}
+//	@Override
+//	public String toString() {
+//		// TODO Auto-generated method stub
+//		return getUid()+getUname()+getUpassword()+getType();
+//	}
+//
+//}
+package com.pzhu.bean;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class User {
-	private	int uid;
-	private	int utelephone;
-	private	String uname;
-	private	String upartment;
-	private	String upassword;
-	private	String type;
-	private	String sex;
-	private	int ugrade;
-	
+	private int uid;
+	private String utelephone;
+	private String uname;
+	private String upartment;
+	private String upassword;
+	private String type;
+	private String sex;
+	private int ugrade;
+
 	public int getUid() {
 		return uid;
 	}
 	public void setUid(int uid) {
 		this.uid = uid;
 	}
-	public int getUtelephone() {
+	public String getUtelephone() {
 		return utelephone;
 	}
-	public void setUtelephone(int utelephone) {
+	public void setUtelephone(String utelephone) {
 		this.utelephone = utelephone;
 	}
 	public String getUname() {
@@ -49,14 +121,12 @@ public class User {
 	public void setType(String type) {
 		this.type = type;
 	}
-	
 	public String getSex() {
 		return sex;
 	}
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
-	
 	public int getUgrade() {
 		return ugrade;
 	}
@@ -65,8 +135,6 @@ public class User {
 	}
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return getUid()+getUname()+getUpassword()+getType();
 	}
-	
 }
--- src/main/java/com/pzhu/mapper/UserMapper.java
+++ src/main/java/com/pzhu/mapper/UserMapper.java
@@ -1,3 +1,78 @@
+//package com.pzhu.mapper;
+//
+//import java.util.List;
+//
+//import org.apache.ibatis.annotations.Delete;
+//import org.apache.ibatis.annotations.Insert;
+//import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Param;
+//import org.apache.ibatis.annotations.Result;
+//import org.apache.ibatis.annotations.Results;
+//import org.apache.ibatis.annotations.Select;
+//import org.apache.ibatis.annotations.Update;
+//
+//import com.pzhu.bean.User;
+//@Mapper
+//public interface UserMapper {
+//	@Results({
+//		@Result(property="uid",column="uid"),
+//		@Result(property="uname",column="uname"),
+//		@Result(property="upassword",column="upassword"),
+//		@Result(property="upartment",column="upartment"),
+//		@Result(property="utelephone",column="utelephone"),
+//		@Result(property="type",column="type"),
+//		@Result(property="sex",column="sex"),
+//		@Result(property="ugrade",column="ugrade")
+//	})
+//
+//	//删除老师
+//	@Delete("DELETE FROM userther WHERE uid = #{uid}")
+//	boolean deleteTechInfo(int uid);
+//
+//	//添加老师INSERT INTO userstu VALUES('2015102','李三','男','1234','2015','数计学院软件工程2班','12345','学生')
+//	@Insert("INSERT INTO userther VALUES(#{uid},#{uname},#{sex},#{upassword},#{upartment},#{utelephone},'学生')")
+//	boolean insertTechInfo(@Param("uid")int uid,@Param("uname")String uname,@Param("sex")String sex,@Param("upassword")String upassword,@Param("upartment")String upartment,@Param("utelephone")int utelephone);
+//
+//	//获取所有老师
+//	@Select("select * FROM userther")
+//	List<User> getTechAll();
+//
+//	//删除学生
+//	@Delete("DELETE FROM userstu WHERE uid = #{uid}")
+//	boolean deleteStuInfo(int uid);
+//
+//	//添加学生INSERT INTO userstu VALUES('2015102','李三','男','1234','2015','数计学院软件工程2班','12345','学生')
+//	@Insert("INSERT INTO userstu VALUES(#{uid},#{uname},#{sex},#{upassword},#{ugrade},#{upartment},#{utelephone},'学生')")
+//	boolean insertStuInfo(@Param("uid")int uid,@Param("uname")String uname,@Param("sex")String sex,@Param("upassword")String upassword,@Param("ugrade")int ugrade,@Param("upartment")String upartment,@Param("utelephone")int utelephone);
+//
+//	//获取所有学生
+//	@Select("select * FROM userstu")
+//	List<User> getStuAll();
+//
+//	@Select("select * FROM userstu WHERE uid = #{uid}")
+//	User getStu(int uid);
+//
+//	@Select("select * FROM userther WHERE uid = #{uid}")
+//	User getTher(int uid);
+//
+//	@Select("select * FROM useradm WHERE uid = #{uid}")
+//	User getAdm(int uid);
+//
+//	@Update("UPDATE userstu SET upassword = #{upassword} WHERE uid = #{uid} and utelephone = #{utelephone} ")
+//	boolean upDateStu(@Param("upassword")String upassword,@Param("uid")int uid,@Param("utelephone")int utelephone);
+//
+//	@Update("UPDATE useradm SET upassword = #{upassword} WHERE uid = #{uid} and utelephone = #{utelephone} ")
+//	boolean upDateAdm(@Param("upassword")String upassword,@Param("uid")int uid,@Param("utelephone")int utelephone);
+//
+//	@Update("UPDATE userther SET upassword = #{upassword} WHERE uid = #{uid} and utelephone = #{utelephone} ")
+//	boolean upDateTher(@Param("upassword")String upassword,@Param("uid")int uid,@Param("utelephone")int utelephone);
+//
+//	//根据课程班级查看学生
+//	@Select("SELECT userstu.uid,uname,sex,ugrade,upartment,utelephone FROM userstu \r\n" +
+//			"INNER JOIN ct_stu ON userstu.uid= ct_stu.uid \r\n" +
+//			"WHERE ct_stu.cid = #{cid} AND ct_stu.classsort =#{classsort} ")
+//	List<User> getmyClassesperson(@Param("cid")int cid,@Param("classsort")int classsort);
+//}
 package com.pzhu.mapper;
 
 import java.util.List;
@@ -12,64 +87,69 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import com.pzhu.bean.User;
+
 @Mapper
 public interface UserMapper {
 	@Results({
-		@Result(property="uid",column="uid"),
-		@Result(property="uname",column="uname"),
-		@Result(property="upassword",column="upassword"),
-		@Result(property="upartment",column="upartment"),
-		@Result(property="utelephone",column="utelephone"),
-		@Result(property="type",column="type"),
-		@Result(property="sex",column="sex"),
-		@Result(property="ugrade",column="ugrade")
+			@Result(property="uid",column="uid"),
+			@Result(property="uname",column="uname"),
+			@Result(property="upassword",column="upassword"),
+			@Result(property="upartment",column="upartment"),
+			@Result(property="utelephone",column="utelephone"),
+			@Result(property="type",column="type"),
+			@Result(property="sex",column="sex"),
+			@Result(property="ugrade",column="ugrade")
 	})
-	
-	//删除老师
-	@Delete("DELETE FROM userther WHERE uid = #{uid}") 
+
+	// Delete teacher
+	@Delete("DELETE FROM userther WHERE uid = #{uid}")
 	boolean deleteTechInfo(int uid);
-	
-	//添加老师INSERT INTO userstu VALUES('2015102','李三','男','1234','2015','数计学院软件工程2班','12345','学生')
+
+	// Add teacher
 	@Insert("INSERT INTO userther VALUES(#{uid},#{uname},#{sex},#{upassword},#{upartment},#{utelephone},'学生')")
-	boolean insertTechInfo(@Param("uid")int uid,@Param("uname")String uname,@Param("sex")String sex,@Param("upassword")String upassword,@Param("upartment")String upartment,@Param("utelephone")int utelephone);
-	
-	//获取所有老师
+	boolean insertTechInfo(@Param("uid")int uid,@Param("uname")String uname,@Param("sex")String sex,
+						   @Param("upassword")String upassword,@Param("upartment")String upartment,
+						   @Param("utelephone")String utelephone);
+
+	// Get all teachers
 	@Select("select * FROM userther")
 	List<User> getTechAll();
-	
-	//删除学生
-	@Delete("DELETE FROM userstu WHERE uid = #{uid}") 
+
+	// Delete student
+	@Delete("DELETE FROM userstu WHERE uid = #{uid}")
 	boolean deleteStuInfo(int uid);
-	
-	//添加学生INSERT INTO userstu VALUES('2015102','李三','男','1234','2015','数计学院软件工程2班','12345','学生')
+
+	// Add student
 	@Insert("INSERT INTO userstu VALUES(#{uid},#{uname},#{sex},#{upassword},#{ugrade},#{upartment},#{utelephone},'学生')")
-	boolean insertStuInfo(@Param("uid")int uid,@Param("uname")String uname,@Param("sex")String sex,@Param("upassword")String upassword,@Param("ugrade")int ugrade,@Param("upartment")String upartment,@Param("utelephone")int utelephone);
-	
-	//获取所有学生
+	boolean insertStuInfo(@Param("uid")int uid,@Param("uname")String uname,@Param("sex")String sex,
+						  @Param("upassword")String upassword,@Param("ugrade")int ugrade,
+						  @Param("upartment")String upartment,@Param("utelephone")String utelephone);
+
+	// Get all students
 	@Select("select * FROM userstu")
 	List<User> getStuAll();
-	
+
 	@Select("select * FROM userstu WHERE uid = #{uid}")
 	User getStu(int uid);
-	
+
 	@Select("select * FROM userther WHERE uid = #{uid}")
 	User getTher(int uid);
-	
+
 	@Select("select * FROM useradm WHERE uid = #{uid}")
 	User getAdm(int uid);
-	
+
 	@Update("UPDATE userstu SET upassword = #{upassword} WHERE uid = #{uid} and utelephone = #{utelephone} ")
-	boolean upDateStu(@Param("upassword")String upassword,@Param("uid")int uid,@Param("utelephone")int utelephone);
-	
+	boolean upDateStu(@Param("upassword")String upassword,@Param("uid")int uid,@Param("utelephone")String utelephone);
+
 	@Update("UPDATE useradm SET upassword = #{upassword} WHERE uid = #{uid} and utelephone = #{utelephone} ")
-	boolean upDateAdm(@Param("upassword")String upassword,@Param("uid")int uid,@Param("utelephone")int utelephone);
-	
+	boolean upDateAdm(@Param("upassword")String upassword,@Param("uid")int uid,@Param("utelephone")String utelephone);
+
 	@Update("UPDATE userther SET upassword = #{upassword} WHERE uid = #{uid} and utelephone = #{utelephone} ")
-	boolean upDateTher(@Param("upassword")String upassword,@Param("uid")int uid,@Param("utelephone")int utelephone);
-	
-	//根据课程班级查看学生
-	@Select("SELECT userstu.uid,uname,sex,ugrade,upartment,utelephone FROM userstu \r\n" + 
-			"INNER JOIN ct_stu ON userstu.uid= ct_stu.uid \r\n" + 
+	boolean upDateTher(@Param("upassword")String upassword,@Param("uid")int uid,@Param("utelephone")String utelephone);
+
+	// Get students of selected class
+	@Select("SELECT userstu.uid,uname,sex,ugrade,upartment,utelephone FROM userstu \r\n" +
+			"INNER JOIN ct_stu ON userstu.uid= ct_stu.uid \r\n" +
 			"WHERE ct_stu.cid = #{cid} AND ct_stu.classsort =#{classsort} ")
 	List<User> getmyClassesperson(@Param("cid")int cid,@Param("classsort")int classsort);
 }
